bd219c689f94f51590113e5bc643eb42517a0b1d
razazulu <razazulu@gmail.com>
2016-01-19 11:43:44 +0000

    Day 17 Exceptions

--- CSharpTestBed/Program.cs
+++ CSharpTestBed/Program.cs
@@ -4,58 +4,41 @@ using System.Linq;
 
 namespace CSharpTestBed
 {
+    class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public int Power(int n, int p)
+        {
+            if(n < 0 || p < 0)throw new Exception("n and p should be non-negative");
+            return (int)Math.Pow(n, p);
+        }
+    }
+
     class Solution
     {
-        private static void Main(String[] args)
+        static void Main(String[] args)
         {
-            //Read input
-            int a = Convert.ToInt32(Console.ReadLine());
-            int[] input = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
-            
-            
-            //This algorithm crashes one of the test cases?
-            //1. sort the array
-            /*int key = 0;
-            int j = 0;
-
-            for (int i = 0; i < input.Length; i++)
+            Calculator myCalculator = new Calculator();
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
             {
-                key = input[i];
-                j = i - 1;
-                while (j >= 0 && key < input[j])
+                string[] num = Console.ReadLine().Split();
+                int n = int.Parse(num[0]);
+                int p = int.Parse(num[1]);
+
+                try
                 {
-
-                    input[j + 1] = input[j];
-                    j--;
+                    int ans = myCalculator.Power(n, p);
+                    Console.WriteLine(ans);
                 }
-                input[j + 1] = key;
-            }*/
-            Array.Sort(input);
-
-            //Find the minimum absolute difference of pairs in this list
-            //int key = 0;
-            //int j = 0;
-            int min = (int) Math.Pow(10, 7);
-
-            for (int i = 0; i < input.Length - 1; i++)
-            {
-                int res = Math.Abs(input[i + 1] - input[i]);
-                if (res < min) min = res;
-            }
-
-            //Find the pairs that correspond to the min difference
-            List<string> output = new List<string>();
-            for (int i = 0; i < input.Length - 1; i++)
-            {
-                if ((Math.Abs(input[i + 1] - input[i])) == min)
+                catch (Exception e)
                 {
-                    output.Add(input[i].ToString() + " " + input[i+1].ToString());
+                    Console.WriteLine(e.Message);
                 }
-            }
-
-            foreach (string s in output)
-            {
-                Console.WriteLine(s);
             }
         }
     }
