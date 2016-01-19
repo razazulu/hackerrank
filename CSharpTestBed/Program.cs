e4cedbf8577965b7226edd9bcc4250d5e63fbab9
razazulu <razazulu@gmail.com>
2016-01-19 12:17:05 +0000

    Day 18 queues/stacks

--- CSharpTestBed/Program.cs
+++ CSharpTestBed/Program.cs
@@ -4,42 +4,74 @@ using System.Linq;
 
 namespace CSharpTestBed
 {
-    class Calculator
+    class Palindrome
     {
-        public Calculator()
-        {
+        Queue<char> queue = new Queue<char>();
+        private Stack<char> stack = new Stack<char>(); 
 
+        public Palindrome()
+        {
+            
         }
 
-        public int Power(int n, int p)
+        public void pushCharacter(char c)
         {
-            if(n < 0 || p < 0)throw new Exception("n and p should be non-negative");
-            return (int)Math.Pow(n, p);
+            stack.Push(c);
+        }
+
+        public void enqueueCharacter(char c)
+        {
+            queue.Enqueue(c);
+        }
+
+        public char popCharacter()
+        {
+            return stack.Pop();
+        }
+
+        public char dequeueCharacter()
+        {
+            return queue.Dequeue();
         }
     }
 
-    class Solution
-    {
-        static void Main(String[] args)
-        {
-            Calculator myCalculator = new Calculator();
-            int T = Int32.Parse(Console.ReadLine());
-            while (T-- > 0)
-            {
-                string[] num = Console.ReadLine().Split();
-                int n = int.Parse(num[0]);
-                int p = int.Parse(num[1]);
-
-                try
-                {
-                    int ans = myCalculator.Power(n, p);
-                    Console.WriteLine(ans);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
+    class Solution {
+    static void Main(String[] args) {
+        // read the string s.
+        string s = Console.ReadLine();
+        
+        // create the Palindrome class object p.
+        Palindrome p = new Palindrome();
+        
+        // push all the characters of string s to stack.
+        foreach (char c in s) {
+            p.pushCharacter(c);
+        }
+        
+        // enqueue all the characters of string s to queue.
+        foreach (char c in s) {
+            p.enqueueCharacter(c);
+        }
+        
+        bool f = true;
+        
+        // pop the top character from stack.
+        // dequeue the first character from queue.
+        // compare both the characters.
+        for (int i = 0; i < s.Length; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                f = false;
+                
+                break;
             }
         }
+        
+        // finally print whether string s is palindrome or not.
+        if (f) {
+            Console.Write("The word, {0}, is a palindrome.", s);
+        } else {
+            Console.Write("The word, {0}, is not a palindrome.", s);
+        }
     }
+}
 }
