32c65d976cbada584c6801a4f48889777c0e647c
razazulu <razazulu@gmail.com>
2016-01-20 11:00:23 +0000

    day 19 - interfaces

--- CSharpTestBed/Program.cs
+++ CSharpTestBed/Program.cs
@@ -1,77 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace CSharpTestBed
 {
-    class Palindrome
+    public interface AdvancedArithmetic
     {
-        Queue<char> queue = new Queue<char>();
-        private Stack<char> stack = new Stack<char>(); 
-
-        public Palindrome()
-        {
-            
-        }
-
-        public void pushCharacter(char c)
-        {
-            stack.Push(c);
-        }
-
-        public void enqueueCharacter(char c)
-        {
-            queue.Enqueue(c);
-        }
-
-        public char popCharacter()
-        {
-            return stack.Pop();
-        }
-
-        public char dequeueCharacter()
-        {
-            return queue.Dequeue();
-        }
+        int divisorSum(int n);
     }
 
-    class Solution {
-    static void Main(String[] args) {
-        // read the string s.
-        string s = Console.ReadLine();
-        
-        // create the Palindrome class object p.
-        Palindrome p = new Palindrome();
-        
-        // push all the characters of string s to stack.
-        foreach (char c in s) {
-            p.pushCharacter(c);
-        }
-        
-        // enqueue all the characters of string s to queue.
-        foreach (char c in s) {
-            p.enqueueCharacter(c);
-        }
-        
-        bool f = true;
-        
-        // pop the top character from stack.
-        // dequeue the first character from queue.
-        // compare both the characters.
-        for (int i = 0; i < s.Length; i++) {
-            if (p.popCharacter() != p.dequeueCharacter()) {
-                f = false;
-                
-                break;
+    class Calculator : AdvancedArithmetic
+    {
+
+        public int divisorSum(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n%i == 0) sum += i;
             }
-        }
-        
-        // finally print whether string s is palindrome or not.
-        if (f) {
-            Console.Write("The word, {0}, is a palindrome.", s);
-        } else {
-            Console.Write("The word, {0}, is not a palindrome.", s);
+            return sum;
         }
     }
-}
+
+    internal class Solution
+    {
+        private static void Main(string[] args)
+        {
+            int n = 6;//Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+        }
+    }
 }
