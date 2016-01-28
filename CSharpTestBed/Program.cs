a15ab9853e2bbc04b2508916cce54b496153961a
razazulu <razazulu@gmail.com>
2016-01-28 14:05:44 +0000

    Day 23 - Binary Search trees 2

--- CSharpTestBed/Program.cs
+++ CSharpTestBed/Program.cs
@@ -1,26 +1,83 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Core.Objects;
 
 namespace CSharpTestBed
 {
     internal class Solution
     {
-        public static void printArray<T>(T[] input)
+        class Node
         {
-            for(int i = 0; i < input.Length; i++)
+            public Node left, right;
+            public int data;
+            public Node(int data)
             {
-                Console.WriteLine(input[i]);
+                this.data = data;
+                left = right = null;
             }
         }
 
-        private static void Main(String[] args)
+        static Node insert(Node root, int data)
         {
-            int[] vInt = new int[] {1, 2, 3};
-            string[] vString = new string[] {"Hello", "World"};
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
 
-            printArray<int>(vInt);
-            printArray<string>(vString);
+
+
+        private static void levelOrder(Node root)
+        {
+            if (root == null) return;
+
+            Queue<Node> queue = new Queue<Node>();
+
+            queue.Clear();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                Node temp = queue.Dequeue();
+                Console.Write(temp.data + " ");
+                if (temp.left != null) queue.Enqueue(temp.left);
+                if (temp.right != null) queue.Enqueue(temp.right);
+            }
+        }
+
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int[] tree = new int[6] { 3, 5, 4, 7, 2, 1 };
+            for (int i = 0; i < tree.Length; i++)
+            {
+                root = insert(root, tree[i]);
+            }
+            //int T = Int32.Parse(Console.ReadLine());
+            //while (T-- > 0)
+            //{
+            //    int data = Int32.Parse(Console.ReadLine());
+            //    root = insert(root, data);
+            //}
+            levelOrder(root);
+
         }
     }
 }
